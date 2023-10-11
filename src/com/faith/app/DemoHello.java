a8d8b4909445feafccfbb6557587a856a810fc90
binuChris <binu.christin@faithinfotech.in>
2023-10-11 17:11:27 +0530

    first commit

---
+++ src/com/faith/app/DemoHello.java
@@ -0,0 +1,36 @@
+package com.faith.app;
+
+import java.util.Scanner;
+
+public class DemoHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Aleena");
+		System.out.print("Alleena");
+		System.out.println();
+		String strOne = scan.nextLine();
+		scan.close();
+		System.out.println("the largest substring appears at both ends:" + atBothEnds(strOne));
+		
+	
+		
+	}
+
+	private static String atBothEnds(String strOne) {
+		// TODO Auto-generated method stub
+		int length = strOne.length(), tempLength=0;
+		String sameString=" ", temp=" ";
+		for(int i=0;i<length;i++){
+			temp+=strOne.charAt(i);
+			tempLength = temp.length();
+			if(i<length/2 && temp.equals(strOne.substring(length-tempLength,length))){
+				sameString=temp;
+			}
+		}
+		return sameString;
+				
+			}
+	}
+
